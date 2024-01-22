60b05b3fc40c9bed0d7d9feebf8a525f9a8205d2
권도현 <tobeykwon@gmail.com>
2024-01-22 18:11:49 +0900

    first commit

---
+++ src/test/java/com/example/datacollectionprocessex/DataCollectionProcessExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datacollectionprocessex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionProcessExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
